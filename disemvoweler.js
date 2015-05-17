61e70b5df4ebaf960d44f011e3cfab7e96e21b6e
Wayne Smith <doeswayneexist@gmail.com>
2015-05-16 20:29:56 -0700

    Added the first version of code to remove vowels.

--- disemvoweler.js
+++ disemvoweler.js
@@ -1,2 +1,24 @@
 var userString = prompt("Type in a sentence.");
-alert(userString);
+
+//take the string and strip it of all vowels
+
+//create a variable to store the new string
+var noVowels = "";
+var vowels = "";
+
+//iterate though each character in the string
+for (var i=0; i<userString.length; i++) {
+	var cL = userString[i];
+	if(cL == 'a' || cL == 'e' || cL == 'i' || cL == 'o' || cL == 'u') {
+		vowels+=cL;
+	} else if (cL == " ") {
+		//do nothing
+	}
+	else {
+		noVowels +=cL;
+	}
+}
+
+alert(noVowels);
+alert(vowels);
+
